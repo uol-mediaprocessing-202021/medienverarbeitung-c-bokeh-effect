400c226ca2f23e0c81f20fbe765e57e39aed4288
Malte Trauernicht <malte.trauernicht@uni-oldenburg.de>
2020-12-21 13:44:19 +0100

    anfang umstellung auf poolnet

--- detection/blur.py
+++ detection/blur.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-import torch
-import torchvision
+from pool_net import PoolNetInterface
 
 
 def torch_blur(source):
@@ -9,36 +8,78 @@ def torch_blur(source):
     img = cv2.imread(source)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-    model.eval()
+    weight_path = "model/no_edge_model.ckpt"
 
-    image2_normalized = img / 255.
-    channels_first = np.moveaxis(image2_normalized, 2, 0)
+    # Load model
+    model = PoolNetInterface(weight_path, device="gpu")
 
-    channels_first = torch.from_numpy(channels_first).float()
+    # Call function
+    mask = model.process(source)
 
-    prediction = model([channels_first])[0]
-    masks = prediction['masks'].detach().numpy()
-    mask = masks[0][0]
+    img = img / 255.  # Normalize image
 
-    image2_box_blurred = cv2.boxFilter(img, -1, (20, 20))
+    cv2.imshow('mask', mask)
 
-    r, g, b = cv2.split(image2_normalized)
+    inverted = np.abs(1. - mask)
+
+    r, g, b = cv2.split(img)
     mr = r * mask
     mg = g * mask
     mb = b * mask
     subject = cv2.merge((mr, mg, mb))
-    subject = np.asarray(subject * 255., dtype='uint8')
 
-    inverted = np.abs(1. - mask)
-    image2_box_blurred_normalized = image2_box_blurred / 255
-    r, g, b = cv2.split(image2_box_blurred_normalized)
     ir = r * inverted
     ig = g * inverted
     ib = b * inverted
     background = cv2.merge((ir, ig, ib))
-    background = np.asarray(background * 255., dtype='uint8')
 
-    final = subject + background
+    subject = np.asarray(subject * 255., dtype='uint8')
 
-    return final
+    background_bokeh = bokeh(np.asarray(background * 255, dtype='uint8'))
+    background_bokeh = np.asarray(background_bokeh * 255, dtype='uint8')
+    combined = cv2.addWeighted(subject, 1., background_bokeh, 1., 0)
+
+    return combined
+
+
+def bokeh(image):
+
+    triangle = np.array([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ], dtype='float')
+
+    mask = triangle
+    kernel = cv2.getGaussianKernel(11, 5)
+    kernel = kernel * kernel.transpose() * mask  # Is the 2D filter
+    kernel = kernel / np.sum(kernel)
+
+    r, g, b = cv2.split(image)
+
+    r = r / 255.
+    g = g / 255.
+    b = b / 255.
+
+    r = np.where(r > 0.9, r * 2, r)
+    g = np.where(g > 0.9, g * 2, g)
+    b = np.where(b > 0.9, b * 2, b)
+
+    fr = cv2.filter2D(r, -1, kernel)
+    fg = cv2.filter2D(g, -1, kernel)
+    fb = cv2.filter2D(b, -1, kernel)
+
+    fr = np.where(fr > 1., 1., fr)
+    fg = np.where(fg > 1., 1., fg)
+    fb = np.where(fb > 1., 1., fb)
+
+    result = cv2.merge((fr, fg, fb))
+    return result
